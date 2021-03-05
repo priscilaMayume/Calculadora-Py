a2f494c04d71ba3a84fb28e3f1c8e073fcf67f98
prisccila <prisccila@gmail.com>
2021-03-05 23:09:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+print ("Usando os operadores aritméticos")
+
+numero1 = input ("Digite o primeiro número: \n")
+numero1 = int(numero1)
+numero2 = input ("Digite o segundo número:\n")
+numero2 = int(numero2)
+
+soma = numero1 + numero2
+print ("A adicao e:" , soma)
+
+subtracao = numero1 - numero2
+print ("A subtracao e:" , subtracao)
+
+multiplicacao = numero1 * numero2
+print ("A multiplicaçao e: ", multiplicacao)
+
+divisao = numero1 / numero2
+print ("A divisao e: ", divisao)
+
+exponenciacao = numero1 ** numero2
+print ("A Exponenciacao e:", exponenciacao)
+
+modulo = numero1 % numero2
+print ("O modulo e: ", modulo)
